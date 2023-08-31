f8e535ae4d1d16cc17f4b9e5b352b7e493781d95
TomF46 <t-d-f@live.co.uk>
2023-08-31 15:43:05 +0100

    GH pages with working base url

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename='/Appathematics-react'>
         <App />
       </BrowserRouter>
     </Provider>
