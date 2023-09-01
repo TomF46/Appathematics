45e87740cbc9ebe42a45b8bd9b44bb7e37b1c4fb
TomF46 <t-d-f@live.co.uk>
2023-09-01 11:29:04 +0100

    More realistic initial high scores to suit new scoring system.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.515buit6mng"
+    "revision": "0.s5j27vnkbb8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
