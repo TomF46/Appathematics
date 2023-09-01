576151b49392a0069e0c6bd5a654f566a0215dbe
TomF46 <t-d-f@live.co.uk>
2023-09-01 14:28:14 +0100

    Icon and image improvements (..webp and maskable icons)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r0duoo0slho"
+    "revision": "0.qq8g894v4sg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
-import logo from "../assets/logo.jpg";
+import logo from "../assets/logo.webp";
 import SetSelect from "../components/SetSelect";
 
 function Home() {
@@ -11,14 +11,14 @@ function Home() {
 
     return (
         <>
-            <img src={logo} className="mx-auto mt-4 logo" />
+            <img src={logo} className="mx-auto mt-4 logo" alt="Appathematics logo" />
             <div className="text-center text-primary">
-                <h2 className="text-4xl">Welcome to Appathematics</h2>
-                <p className="text-lg">
+                <h2 className="text-2xl md:text-4xl mb-4">Welcome to Appathematics</h2>
+                <p className="text-base md:text-lg">
                     Choose your level from the drop down below and click the
                     start button to begin
                 </p>
-                <p className="text-lg">
+                <p className="text-base md:text-lg">
                     The higher the number of questions the harder the possible
                     questions become
                 </p>
