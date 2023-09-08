75b863149d13f951b123788a66b89b04d9a82e8d
TomF46 <t-d-f@live.co.uk>
2023-09-08 10:24:51 +0100

    Tests to ensure Scores leaderboard renders correct data

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
+define(['./workbox-4abc5dbb'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nj2ahtvjep"
+    "revision": "0.bh0m9714hdo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
